ff203c8c6f376e434d07290a05b4a1e7d8622f79
ALE <93939843+Lionetto96@users.noreply.github.com>
2022-05-25 17:19:05 +0200

    Add files via upload

---
+++ GestioneOrdini_Mock/RepositoriesMock/MockRepoCliente.cs
@@ -0,0 +1,77 @@
+﻿using GestioneOrdini_CORE.Interfaces;
+using GestioneOrdini_CORE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestioneOrdini_Mock.RepositoriesMock
+{
+    public class MockRepoCliente : IClienteRepository
+    {
+        private List<Cliente> _clienteList = new List<Cliente>()
+        {
+            new Cliente()
+            {
+                Id = 1,
+                CodiceCliente ="C001",
+                Nome ="alessia",
+                Cognome="Lionetto"
+            },
+            new Cliente()
+            {
+                Id=2,
+                CodiceCliente ="C002",
+                Nome="piero",
+                Cognome="rossi"
+            }
+        };
+        public bool Add(Cliente entity)
+        {
+            var exist=_clienteList.Exists(c=>c.Id == entity.Id);
+            if(exist)
+                throw new Exception($"Customer with id {entity.Id} alredy exist");
+            _clienteList.Add(entity);
+            return true;
+        }
+
+        public bool Delete(Cliente entity)
+        {
+            var exist = _clienteList.Exists(c => c.Id == entity.Id);
+            if (!exist)
+                throw new Exception($"Customer with id {entity.Id} not exist");
+            _clienteList.Remove(entity);
+            return true;
+        }
+
+        public IEnumerable<Cliente> GetAll()
+        {
+            return _clienteList;
+        }
+
+        public Cliente GetClienteByCode(string codiceCliente)
+        {
+            return _clienteList.SingleOrDefault(c=>c.CodiceCliente==codiceCliente);
+           
+        }
+
+        public Cliente GetClienteById(int id)
+        {
+            return _clienteList.SingleOrDefault(c => c.Id == id);
+        }
+
+        public bool Update(Cliente entity)
+        {
+            var exist = _clienteList.Exists(c => c.Id == entity.Id);
+            if (!exist)
+                throw new Exception($"Customer with id {entity.Id} not exist");
+            var upC=_clienteList.SingleOrDefault(c=>c.Id == entity.Id);
+            if (upC != null)
+            {
+                upC.Nome =entity.Nome;
+            }
+            
+            return true;
+        }
+    }
+}
---
+++ GestioneOrdini_Mock/RepositoriesMock/MockRepoOrdine.cs
@@ -0,0 +1,77 @@
+﻿using GestioneOrdini_CORE.Interfaces;
+using GestioneOrdini_CORE.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestioneOrdini_Mock.RepositoriesMock
+{
+    public class MockRepoOrdine : IOrdineRepository
+        
+    {
+        private List<Ordine> _ordini = new List<Ordine>()
+        {
+            new Ordine()
+            {
+                Id=1,
+                DataOrdine=new DateTime(2022,01,30),
+                CodiceOrdine="CO001",
+                CodiceProdotto="CP001",
+                Importo=189,
+                IdCliente=1
+            },
+             new Ordine()
+            {
+                Id=2,
+                DataOrdine=new DateTime(2022,01,28),
+                CodiceOrdine="CO002",
+                CodiceProdotto="CP002",
+                Importo=189,
+                IdCliente=2
+            }
+        };
+        public bool Add(Ordine entity)
+        {
+            var exist = _ordini.Exists(c => c.Id == entity.Id);
+            if (exist)
+                throw new Exception($"Order with id {entity.Id} alredy exist");
+            _ordini.Add(entity);
+            return true;
+        }
+
+        public bool Delete(Ordine entity)
+        {
+            var exist = _ordini.Exists(c => c.Id == entity.Id);
+            if (!exist)
+                throw new Exception($"Order with id {entity.Id} not exist");
+            _ordini.Remove(entity);
+            return true;
+        }
+
+        public IEnumerable<Ordine> GetAll()
+        {
+            return _ordini;
+        }
+
+        public Ordine GetOrdineById(int id)
+        {
+            return _ordini.SingleOrDefault(o => o.Id == id);
+        }
+
+        public IEnumerable<Ordine> GetOrdiniByIdCliente(int idCliente)
+        {
+            return (IEnumerable<Ordine>)_ordini.SingleOrDefault(_ordini=>_ordini.IdCliente == idCliente);
+        }
+
+        public bool Update(Ordine entity)
+        {
+            var exist = _ordini.Exists(c => c.Id == entity.Id);
+            if (!exist)
+                throw new Exception($"Order with id {entity.Id} not exist");
+
+            entity.Importo = 160;
+            return true;
+        }
+    }
+}
